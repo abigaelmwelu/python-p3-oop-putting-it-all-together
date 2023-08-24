2e3832d3a687d32edf1b4af0e7a1595268deade0
abigael mwelu <abigael.mutuku@student.moringaschool.com>
2023-08-24 15:13:02 +0300

    completed assingment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,41 @@
-#!/usr/bin/env python3
 
 class Book:
     pass
+class Book:
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = page_count
+
     
-        
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+class TestBookMethods():
+    def test_requires_int_page_count(self):
+        '''prints "page_count must be an integer" if page_count is not an integer.'''
+        book = Book("And Then There Were None", 272)
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        book.page_count = "not an integer"
+        sys.stdout = sys.__stdout__
+        assert captured_out.getvalue() == "page_count must be an integer\n"
+
+    def test_turn_page(self):
+        '''tests turning pages'''
+        book = Book("Sample Book", 5)
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        book.turn_page()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(captured_out.getvalue(), "Flipping the page...wow, you read fast!\n")
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
+
+
--- lib/shoe.py
+++ lib/shoe.py
@@ -2,3 +2,30 @@
 
 class Shoe:
     pass
+class Shoe:
+   def __init__(self, brand, size):
+      self.size= size 
+      self.brand= brand
+      
+      import io
+import sys
+import unittest
+from shoe import Shoe
+
+class TestShoeMethods(unittest.TestCase):
+    def test_requires_int_size(self):
+        '''prints "size must be an integer" if size is not an integer.'''
+        stan_smith = Shoe("Adidas", 9)
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        stan_smith.size = "not an integer"
+        sys.stdout = sys.__stdout__
+        self.assertEqual(captured_out.getvalue(), "size must be an integer\n")
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+      
+
+       
